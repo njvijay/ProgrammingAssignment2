c41b45a3a56a06280614a2041f765b8eada9d91b
Vijayakumar Jawaharlal <vijayakumar.jawaharlal@gmail.com>
2014-04-21 20:57:01 -0400

    Added comments to explain the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+# title : R progamming Assignment 2
+# Author : Vijayakumar Jawaharlal
 
-## Write a short comment describing this function
+# This is second programming assignment on R programming. This class is conducted by 
+# John Hopkins university via coursera. Intention of the this assignment is, working with
+# function. Additionally, cache the value of the matrix inverse so that when we need it again, 
+# it can be looked up in the cache rather than recomputed. 
+
+## makeCacheMatrix function is collection of getter setter functions to set and get
+# matrix and set and get matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -22,7 +28,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - The following function calculates the mean of the special "matrix" created with the above 
+## function. However, it first checks to see if the matrix inverse has already been calculated. 
+## If so, it gets the matrix inverse from the cache and skips the computation. 
+## Otherwise, it calculates the matrix inverse and sets the value of the mean in the cache via the 
+## setinv function.
 
 cacheSolve <- function(x, ...) {
     m <- x$getinv()
@@ -37,3 +47,11 @@ cacheSolve <- function(x, ...) {
     m
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Here is how above functions are put to use
+## a <- makeCacheMatrix(x=matrix(1:4,2,2))
+##  d <- cacheSolve(x=a) 
+## Try again: d <- cacheSolve(x=a) -- this should get cached data. Look for print "getting cached data"
+## d
+## above should print the inverse of the matrix which was assigned for 'a'
+
